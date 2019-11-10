b9ed0e3daf3759bebd25a691a2f2b83facc6631c
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-10 11:32:21 +0000

    1.20191110.1

--- CSAc4yEntityLibraryTester/Program.cs
+++ CSAc4yEntityLibraryTester/Program.cs
@@ -1,36 +1,52 @@
 ﻿
+using CSAc4yClass.Class;
 using d7p4n4.EFMethods.Class;
 using d7p4n4.Final.Class;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace CSAc4yEntityLibraryTester
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            //Ac4yIdentificationBaseEntityMethods ac4YIdentificationBaseEntityMethods = new Ac4yIdentificationBaseEntityMethods("Ac4yDb2");
-            Ac4yEntityMethods ac4YEntityMethods = new Ac4yEntityMethods("Ac4yDb2");
+        { 
+            Ac4yChildEntityMethods ac4YEntityMethods = new Ac4yChildEntityMethods("Ac4yDb2");
 
-            Ac4y ac4Y = new Ac4y()
+            Ac4yChild ac4yChild = new Ac4yChild()
             {
-                HumanId = "ac4yobject",
-                serialization = "<?xml version='1.0' encoding=\"UTF - 8\"?>< Ac4yClass xmlns:xsd = \"http://www.w3.org/2001/XMLSchema\" xmlns: xsi = \"http://www.w3.org/2001/XMLSchema-instance\" >< Name > Ac4y </ Name >< Ancestor > Ac4yIdentification </ Ancestor >< Namespace > CSAc4y.Class </ Namespace >< GUID />< PropertyList >< Ac4yProperty >< Name > serialization </ Name >< Type > String </ Type ></ Ac4yProperty >< Ac4yProperty >< Name > template </ Name >< Type > Ac4yIdentificationBase </ Type ></ Ac4yProperty >< Ac4yProperty >< Name > GUID </ Name >< Type > String </ Type ></ Ac4yProperty >< Ac4yProperty >< Name > HumanId </ Name >< Type > String </ Type ></ Ac4yProperty >< Ac4yProperty >< Name > PublicHumanId </ Name >< Type > String </ Type ></ Ac4yProperty >< Ac4yProperty >< Name > id </ Name >< Type > Int32 </ Type ></ Ac4yProperty ></ PropertyList ></ Ac4yClass > ",
+                FirstName = "Jack",
+                HumanId = "ac4yChild",
+                serialization = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Ac4yClass xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><Name>Ac4yChild</Name><Ancestor>Ac4y</Ancestor><Namespace>CSAc4yObjectService.Class</Namespace><GUID /><PropertyList><Ac4yProperty><Name>FirstName</Name><Type>String</Type></Ac4yProperty><Ac4yProperty><Name>LastName</Name><Type>String</Type></Ac4yProperty></PropertyList></Ac4yClass>",
                 template = new Ac4yIdentificationBase()
                 {
                     HumanId = "ac4ytemplate",
                 }
             };
 
-            ac4YEntityMethods.addNew(ac4Y);
+            //ac4YEntityMethods.addNew(ac4yChild);
+            
+            Ac4yChild ac4yChild2 = ac4YEntityMethods.findFirstById(6);
+            Console.WriteLine(ac4yChild2.FirstName);
+
+            string xml = ac4yChild2.serialization;
+            Console.WriteLine(ac4yChild2.serialization);
+
+            Ac4yClass ac4yChild3 = null;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Ac4yClass));
+
+            StringReader reader = new StringReader(xml);
+            ac4yChild3 = (Ac4yClass)serializer.Deserialize(reader);
+            reader.Close();
 
 
-            Ac4y uj = ac4YEntityMethods.findFirstById(1);
-            Console.WriteLine(uj.GUID);
+            
         }
     }
 }
