f2b41b0ab5b767a5fd12c81e0aa7644bf773a016
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-10 20:22:57 +0000

    1.20191110.3

---
+++ CSAc4yEntityLibraryTester/LoadAndSaveXML.cs
@@ -0,0 +1,52 @@
+﻿using d7p4n4Namespace.EFMethods.Class;
+using d7p4n4Namespace.Final.Class;
+using d7p4n4Namespace.PreProcessed.Class;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CSAc4yEntityLibraryTester
+{
+    class LoadAndSaveXML
+    {
+        public Ac4ySDSequence LoadXmlById(int id, Ac4ySDSequenceEntityMethods ySDSequenceEntityMethods)
+        {
+            Ac4ySDSequence ac4YSDSequence = ySDSequenceEntityMethods.findFirstById(id);
+
+            string xml = ac4YSDSequence.serialization;
+
+            Ac4ySDSequence ySDSequence = null;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Ac4ySDSequence));
+
+            StringReader reader = new StringReader(xml);
+            ySDSequence = (Ac4ySDSequence)serializer.Deserialize(reader);
+            reader.Close();
+
+            return ySDSequence;
+        }
+
+        public void SaveWithXml(Ac4ySDSequence ySDSequence, Ac4ySDSequenceEntityMethods ySDSequenceEntityMethods)
+        {
+            string xml = "";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Ac4ySDSequence));
+            StringWriter stringWriter = new StringWriter();
+            XmlWriter xmlWriter = XmlWriter.Create(stringWriter);
+
+            serializer.Serialize(xmlWriter, ySDSequence);
+
+            xml = stringWriter.ToString();
+
+            ySDSequence.serialization = xml;
+
+            ySDSequenceEntityMethods.addNew(ySDSequence);
+        }
+
+    }
+}
--- CSAc4yEntityLibraryTester/Program.cs
+++ CSAc4yEntityLibraryTester/Program.cs
@@ -2,6 +2,8 @@
 using CSAc4yClass.Class;
 using d7p4n4.EFMethods.Class;
 using d7p4n4.Final.Class;
+using d7p4n4Namespace.EFMethods.Class;
+using d7p4n4Namespace.Final.Class;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,7 +17,35 @@ namespace CSAc4yEntityLibraryTester
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            Ac4ySDSequenceEntityMethods ac4YSDSequenceEntityMethods = new Ac4ySDSequenceEntityMethods("Ac4ySD2");
+
+            Ac4ySDSequence sequence = new Ac4ySDSequence()
+            {
+                Communication = new List<Ac4ySDCommunication>()
+               {
+                   new Ac4ySDCommunication()
+                   {
+                       Sender = new Ac4ySDParticipant(){ Name = "frontend" }
+                       ,Receiver = new Ac4ySDParticipant(){ Name = "backend" }
+                       ,Message = new Ac4ySDMessage(){ Message = "login" }
+                   }
+                   ,new Ac4ySDCommunication()
+                   {
+                       Sender = new Ac4ySDParticipant(){ Name = "backend" }
+                       ,Receiver = new Ac4ySDParticipant(){ Name = "frontend" }
+                       ,Message = new Ac4ySDMessage(){ Message = "ok" }
+                   }
+               }
+            };
+
+            LoadAndSaveXML lasx = new LoadAndSaveXML();
+
+            //lasx.SaveWithXml(sequence, ac4YSDSequenceEntityMethods);
+
+            lasx.LoadXmlById(10, ac4YSDSequenceEntityMethods);
+
+            /*
             Ac4yChildEntityMethods ac4YEntityMethods = new Ac4yChildEntityMethods("Ac4yDb2");
 
             Ac4yChild ac4yChild = new Ac4yChild()
@@ -30,7 +60,7 @@ namespace CSAc4yEntityLibraryTester
             };
 
             //ac4YEntityMethods.addNew(ac4yChild);
-            
+            /*
             Ac4yChild ac4yChild2 = ac4YEntityMethods.findFirstById(6);
             Console.WriteLine(ac4yChild2.FirstName);
 
@@ -44,9 +74,9 @@ namespace CSAc4yEntityLibraryTester
             StringReader reader = new StringReader(xml);
             ac4yChild3 = (Ac4yClass)serializer.Deserialize(reader);
             reader.Close();
+            */
 
 
-            
         }
     }
 }
